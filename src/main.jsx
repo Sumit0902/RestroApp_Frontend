ba2ee6aba48e5b5c3c963b0787eaad2fdbb2d19e
Sumit0902 <sumit398383@gmail.com>
2025-01-20 04:17:54 +0500

    merge 19 jan

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,6 @@ createRoot(document.getElementById('root')).render(
       <PersistGate loading={null} persistor={persistor}>
         <Theme appearance='light' accentColor='gray' panelBackground="translucent">
           <App />
-          
         </Theme>
       </PersistGate>
     </Provider>
